48082bce6e5e9d00dda79211f6dd5340c28ba903
kodi6 <punniyakodi.office@gmail.com>
2024-09-26 17:48:22 +0000

    Add school schema

---
+++ priv/repo/migrations/20240926174625_create_students.exs
@@ -0,0 +1,20 @@
+defmodule TrackMyClass.Repo.Migrations.CreateStudents do
+  use Ecto.Migration
+
+  def change do
+    create table(:students, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :first_name, :string
+      add :last_name, :string
+      add :enrollment_number, :string
+      add :dob, :date
+      add :gender, :string
+      add :school_id, references(:schools, on_delete: :nothing, type: :binary_id)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:students, [:enrollment_number])
+    create index(:students, [:school_id])
+  end
+end
