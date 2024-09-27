342cf6d81f0626c1c97f5db388cdaf21c4eb9522
kodi6 <punniyakodi.office@gmail.com>
2024-09-27 16:48:29 +0000

    Add attendance schema

---
+++ priv/repo/migrations/20240926193206_create_attendances.exs
@@ -0,0 +1,17 @@
+defmodule TrackMyClass.Repo.Migrations.CreateAttendances do
+  use Ecto.Migration
+
+  def change do
+    create table(:attendances, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :date, :date
+      add :forenoon_present, :boolean, default: true, null: false
+      add :afternoon_present, :boolean, default: true, null: false
+      add :student_id, references(:students, on_delete: :nothing, type: :binary_id)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:attendances, [:student_id])
+  end
+end
