17763e83148e9e28aa2ca4f6d2571ba3837fa693
kodi6 <punniyakodi.office@gmail.com>
2024-09-27 17:40:27 +0000

    add list schools function

--- lib/track_my_class/schools.ex
+++ lib/track_my_class/schools.ex
@@ -2,4 +2,12 @@ defmodule TrackMyClass.Schools do
   import Ecto.Query, warn: false
   alias TrackMyClass.Repo
 
+  alias TrackMyClass.Schools.{
+    School
+  }
+
+  def list_schools do
+    Repo.all(School)
+  end
+
 end
