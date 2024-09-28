029c807894368647f316fa5cb8a2d8a093e4550d
kodi6 <punniyakodi.office@gmail.com>
2024-09-28 02:48:00 +0000

    Implement create action for SchoolController

--- lib/track_my_class_web/controllers/api/school_controller.ex
+++ lib/track_my_class_web/controllers/api/school_controller.ex
@@ -1,6 +1,7 @@
 defmodule TrackMyClassWeb.Api.SchoolController do
   use TrackMyClassWeb, :controller
 
+  alias TrackMyClass.Schools.School
   alias TrackMyClass.Schools
 
   def index(conn, _params) do
@@ -11,5 +12,13 @@ defmodule TrackMyClassWeb.Api.SchoolController do
   def show(conn, %{"id" => id}) do
     school = Schools.get_school!(id)
     render(conn, :show, school: school)
+  end
+
+  def create(conn, %{"school" => school_params}) do
+    with {:ok, %School{} = school} <- Schools.create_school(school_params) do
+      conn
+      |> put_status(:created)
+      |> render(:show, school: school)
+    end
   end
 end
